437fc0dd6c806de0ae07e5a28b469c156c5e48c6
Almir Zulic <almir@bind.ba>
2019-04-19 16:03:38 +0200

    primjer

--- javascript/login.mjs
+++ javascript/login.mjs
@@ -1,25 +1,32 @@
-const token;
+let token;
 
-function loginRequest (){
+ const loginRequest = (email, password) =>  new Promise((resolve, reject) => {
     let url = 'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/user/login';
     let xhttp = new XMLHttpRequest ();
-    let object = {};
-    request.email = document.getElementById('email').value;
-    request.password = document.getElementById('password').value;
+    let object = {
+        email,
+        password
+    };
     let request = JSON.stringify(object);
     xhttp.onreadystatechange = function (){
         if(xhttp.readyState ==4 && xhttp.status == 200){
-            document.getElementById('log').innerHTML = xhttp.responseText;
-            let response = JSON.parse(xhttp.responseText);
-            document.getElementById ('result').innerHTML = response.token;
             token = JSON.parse(xhttp.responseText);
-
+            resolve(token);
         }
         if (xhttp.readyState == 4 && xhttp.status != 200) {
-            document.getElementById ('error').innerHTML = 'Incorrect mail or password';
+            reject('Incorrect mail or password');
         }
     }
     xhttp.open ('POST', url, true);
     xhttp.setRequestHeader ("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(request);
+});
+
+const isLoggedIn = () => {
+    let token = localStorage.getItem('token');
+    return token;
 }
+
+export { isLoggedIn, loginRequest };
+
+
